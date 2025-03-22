79d36e1d02444b87d8a2ab2d893eac0fe1eca1e7
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-03-22 10:28:55 +0900

    ✨ cors 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.apps.router import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(router=api_router)
