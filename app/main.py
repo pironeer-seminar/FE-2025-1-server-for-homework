0632bc3af45761466850a888f87074a818be7d4f
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-04-06 13:52:40 +0900

    🚀 cors 설정에서 허용된 origin 추가

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+            "http://localhost:5173",
+            "https://homework-fe-2025-1-week3-navy.vercel.app",
+            "https://homework-fe-2025-1-week3-navy.vercel.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
